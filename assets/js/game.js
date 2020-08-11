d940d81f7a27b608bade14b2be1d42e2230b5356
Jeff Johnston <jeffwjohn@yahoo.com>
2020-08-11 09:57:46 -0500

    fix blank name bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -71,6 +71,7 @@ var startGame = function () {
         if (playerInfo.health > 0) {
             // let user know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
             window.alert("Round " + (i + 1));
+            debugger;
 
             // pick new enemy to fight based on the index of the enemy.names array
             var pickedEnemyObj = enemyInfo[i];
@@ -177,9 +178,21 @@ var randomNumber = function (min, max) {
     return value;
 };
 
+// function to set name
+var getPlayerName = function() {
+    var name = "";
+  
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+      }
+  
+    console.log("Your robot's name is " + name + ".");
+    return name;
+  };
 
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    //name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
